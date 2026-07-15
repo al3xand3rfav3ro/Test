4afdec7a83e8ab7a68891cbbdccb340b1fdcc66a
al3xand3rfav3ro <alexanderfavero@btinternet.com>
2026-07-16 00:12:18 +0200

    Add concert data to script.js

---
+++ GigFinder/script.js
@@ -0,0 +1,37 @@
+const concerts = [
+
+{
+    artist: "Metallica",
+    city: "Milan",
+    price: 60,
+    date: "12 Aug 2026",
+    spotify: "https://open.spotify.com/search/Metallica"
+},
+
+{
+    artist: "Architects",
+    city: "Berlin",
+    price: 35,
+    date: "22 Sep 2026",
+    spotify: "https://open.spotify.com/search/Architects"
+},
+
+{
+    artist: "Bring Me The Horizon",
+    city: "London",
+    price: 80,
+    date: "3 Oct 2026",
+    spotify: "https://open.spotify.com/search/Bring+Me+The+Horizon"
+},
+
+{
+    artist: "Sleep Token",
+    city: "Paris",
+    price: 45,
+    date: "15 Nov 2026",
+    spotify: "https://open.spotify.com/search/Sleep+Token"
+}
+
+];
+
+const bookmarks = [];
